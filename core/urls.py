67ba5b4cffa5ce8f56724dff32b0d54e874ee2da
fnabiyevuz <fnabiyevuz@gmail.com>
2023-03-30 14:08:14 +0500

    feat: sign in, sign up and sign out were created

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    path("user/", include('apps.user.urls')),
+    path("story/", include('apps.story.urls')),
     path("admin/", admin.site.urls),
 ]
