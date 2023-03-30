b7cd1adddda00a12976abf77721bd7f12de03e03
Otabek <otabekrahmonovtest@gmail.com>
2023-03-30 23:30:26 +0500

    feat: signallar notification yaratish uchun

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from apps.user.models import CustomUser
+from apps.user.models import CustomUser, UserFollower
 
 admin.site.register(CustomUser)
+admin.site.register(UserFollower)
