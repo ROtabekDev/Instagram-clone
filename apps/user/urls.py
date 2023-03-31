f9689f44a9d76b9a5bb6aaea7dc3a324e8edc7cd
fnabiyevuz <fnabiyevuz@gmail.com>
2023-03-31 14:35:46 +0500

    feat: profile page

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import sign_in, sign_up, sign_out, ProfileView
+from .views import sign_in, sign_up, sign_out, profile
 
 urlpatterns = [
     path("sign-in/", sign_in, name='sign-in'),
     path("sign-up/", sign_up, name='sign-up'),
     path("sign-out/", sign_out, name='sign-out'),
 
-    path("profile/", ProfileView.as_view(), name='profile'),
+    path("<str:username>/", profile, name='profile'),
 ]
