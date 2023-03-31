08982ab03d9dc8a0cbe484bad6114281401d2af9
fnabiyevuz <fnabiyevuz@gmail.com>
2023-03-31 18:18:22 +0500

    feat: likes, comments, add-comment at homepage

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, add_comment
 
 urlpatterns = [
     path("", HomeView.as_view(), name='home'),
+    path("add-comment/", add_comment, name='add-comment'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import TemplateView
+import json
 
+from django.views.generic import TemplateView
+from django.http import JsonResponse
+
+from apps.main.models import Comment
 from apps.post.models import Post
 
 
@@ -8,5 +12,19 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all().exclude(user_id=self.request.user)
+        context['posts'] = Post.objects.filter(user_id__following__follower=self.request.user).exclude(
+            user_id=self.request.user)
         return context
+
+
+def add_comment(request):
+    data = json.loads(request.body)
+    post_id = data['post_id']
+    comment = data['comment']
+    comment = Comment.objects.create(user=request.user, post_id_id=post_id, text=comment)
+    data = {
+        "post_id": comment.post_id_id,
+        "user": comment.user.username,
+        "comment": comment.text
+    }
+    return JsonResponse(data)
